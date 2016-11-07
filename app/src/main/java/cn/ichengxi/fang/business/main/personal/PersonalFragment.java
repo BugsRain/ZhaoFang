0b135edcebeea957a704fe4f5afa41296be1b871
BmobSnail <409962004@qq.com>
2016-11-07 15:38:30 +0800

    我的模块相关-snail commit

--- app/src/main/java/cn/ichengxi/fang/business/main/personal/PersonalFragment.java
+++ app/src/main/java/cn/ichengxi/fang/business/main/personal/PersonalFragment.java
@@ -1,8 +1,10 @@
 package cn.ichengxi.fang.business.main.personal;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 
+import butterknife.Bind;
 import cn.ichengxi.fang.R;
 import cn.ichengxi.fang.frame.base.BaseFrameFragment;
 
@@ -12,7 +14,20 @@ import cn.ichengxi.fang.frame.base.BaseFrameFragment;
 
 public class PersonalFragment extends BaseFrameFragment {
 
-    private LinearLayout mTabContain;
+    @Bind(R.id.personal_collect_layout)
+    LinearLayout mCollectLayout;
+
+    @Bind(R.id.personal_house_layout)
+    LinearLayout mHouseLayout;
+
+    @Bind(R.id.personal_about_layout)
+    LinearLayout mAboutLayout;
+
+    @Bind(R.id.personal_feedback_layout)
+    LinearLayout mFeedbackLayout;
+
+    @Bind(R.id.personal_setting_layout)
+    LinearLayout mSettingLayout;
 
     @Override
     public void onSetView(Bundle savedInstanceState) {
@@ -22,7 +37,32 @@ public class PersonalFragment extends BaseFrameFragment {
     @Override
     public void initView() {
         super.initView();
-        setTitle("我的");
+        mCollectLayout.setOnClickListener(this);
+        mHouseLayout.setOnClickListener(this);
+        mAboutLayout.setOnClickListener(this);
+        mFeedbackLayout.setOnClickListener(this);
+        mSettingLayout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.personal_collect_layout:
+                break;
+
+            case R.id.personal_house_layout:
+                break;
+
+            case R.id.personal_about_layout:
+                break;
+
+            case R.id.personal_feedback_layout:
+                break;
+
+            case R.id.personal_setting_layout:
+                break;
+        }
     }
 
     @Override
@@ -34,4 +74,5 @@ public class PersonalFragment extends BaseFrameFragment {
     public void onRequestEnd() {
 
     }
+
 }
