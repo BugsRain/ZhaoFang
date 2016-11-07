a7f1ce7a197836d4baeecb7f8d9ae30dd44bf11c
quan <382535912@qq.com>
2016-11-07 14:55:44 +0800

    完成首页

--- app/src/main/java/cn/ichengxi/fang/business/main/home/HomeFragment.java
+++ app/src/main/java/cn/ichengxi/fang/business/main/home/HomeFragment.java
@@ -1,8 +1,10 @@
 package cn.ichengxi.fang.business.main.home;
 
 import android.os.Bundle;
+import android.view.View;
 
 import cn.ichengxi.fang.R;
+import cn.ichengxi.fang.business.house_detail.HouseDetailActivity;
 import cn.ichengxi.fang.frame.base.BaseFrameFragment;
 
 /**
@@ -14,6 +16,17 @@ public class HomeFragment extends BaseFrameFragment {
     @Override
     public void onSetView(Bundle savedInstanceState) {
         setmContentView(R.layout.fragment_home);
+    }
+
+    @Override
+    public void initView() {
+        super.initView();
+        findViewByIdToView(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity(HouseDetailActivity.class);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/cn/ichengxi/fang/view/BaseRecyclerView.java
@@ -0,0 +1,25 @@
+package cn.ichengxi.fang.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by quan on 16/11/7.
+ */
+
+public class BaseRecyclerView extends RecyclerView {
+
+    public BaseRecyclerView(Context context) {
+        super(context);
+    }
+
+    public BaseRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BaseRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
